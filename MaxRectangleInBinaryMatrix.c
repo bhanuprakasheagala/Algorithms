4c06fb4e5b51b65c9842bf014b5120d5c66683db
Bhanuprakash Eagala <31948251+bhanuprakasheagala@users.noreply.github.com>
2022-01-07 19:03:41 +0530

    From HackerRank

---
+++ MaxRectangleInBinaryMatrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n;
+	int i,j,count=0;
+	scanf("%d",&n);
+	int a[n][n];
+	for(i=0;i<n;++i)
+	{
+		for(j=0;j<n;++j)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<n;++i)
+	{
+		for(j=0;j<n;++j)
+		{
+			
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
