8b564ad93c53978de1aa7241b2fab8167e14ce07
Bhanuprakash Eagala <31948251+bhanuprakasheagala@users.noreply.github.com>
2022-01-17 19:21:10 +0530

    From Coursera Assignment
    Solved in Algorithmic Tool Box course in Coursera. This Problem is from Week3 Assignment question1, which falls under the category of Greedy technique.

---
+++ change.cpp
@@ -0,0 +1,48 @@
+/*The goal in this problem is to find the minimum number of coins needed to change the input value
+(an integer) into coins with denominations 1, 5, and 10.
+
+Input Format. The input consists of a single integer 𝑚.
+Constraints. 1 ≤ 𝑚 ≤ 10 3 .
+Output Format. Output the minimum number of coins with denominations 1, 5, 10 that changes 𝑚.
+*/
+
+#include <iostream>
+
+int total_count = 0;
+
+int get_change(int m) {
+  //write your code here
+  int d;
+  if(m <= 4)
+    total_count = m;
+
+  else if(m == 10 || m == 5){
+    total_count = 1;
+  }
+
+  else if(m > 5 && m < 10){
+      total_count = (m%5)+1;
+
+  }
+
+  else{
+    int t = m;
+    m = m/10;
+    d = t%10;
+
+    if(d == 0){
+      total_count = m;
+    }
+    else{
+      total_count = m + (d%10)%5+1;
+    }
+  }
+
+  return total_count;
+}
+
+int main() {
+  int m;
+  std::cin >> m;
+  std::cout << get_change(m) << '\n';
+}
