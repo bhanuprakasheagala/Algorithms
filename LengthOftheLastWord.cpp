1912979231903f3c5e901998b3b665941da185c6
Bhanuprakash Eagala <31948251+bhanuprakasheagala@users.noreply.github.com>
2022-01-21 07:19:02 +0530

    From LeetCode
    There is no main method, Only Solution method is there, which I just used the same code where I have written it in LeetCode.

---
+++ LengthOftheLastWord.cpp
@@ -0,0 +1,47 @@
+/*
+Given a string s consisting of some words separated by some number of spaces, 
+return the length of the last word in the string.
+A word is a maximal substring consisting of non-space characters only.
+
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+
+*/
+
+/*This Code stub written by Bhanuprakash_Eagala and from LeetCode*/
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = s.size();
+        int count = 0;
+        for(int i=len-1; i>=0; i--){
+            if(s[i] != ' '){
+                count++;
+            }
+            else if(s[i] == ' '){
+                
+                if(count > 0){
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
