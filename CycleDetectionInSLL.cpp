435924a1954ee96ca3fa841f1e1ce61dc117a9be
Bhanuprakash Eagala <31948251+bhanuprakasheagala@users.noreply.github.com>
2022-02-13 17:51:11 +0530

    From LeetCode, LinkedList Concept

---
+++ CycleDetectionInSLL.cpp
@@ -0,0 +1,36 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
+Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+@LeetCode
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *p1 = head;
+        ListNode *p2 = head;
+        
+        while(p1 != nullptr && p2 != nullptr && p2->next != nullptr){
+            p1 = p1->next;
+            p2 = p2->next->next;
+            
+            if(p2 == p1)
+                return true;
+        }
+        return false;
+    }
+};
