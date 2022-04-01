90dc5195a76dafde4c2f5aa108526575292a099a
Bhanuprakash Eagala <31948251+bhanuprakasheagala@users.noreply.github.com>
2022-04-01 07:05:10 +0530

    From LeetCode

---
+++ GuessTheNumberHigherOrLower.cpp
@@ -0,0 +1,63 @@
+/*
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+    -1: Your guess is higher than the number I picked (i.e. num > pick).
+    1: Your guess is lower than the number I picked (i.e. num < pick).
+    0: your guess is equal to the number I picked (i.e. num == pick).
+
+Return the number that I picked.
+
+ 
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+
+@LeetCode - BinarySearch
+*/
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 0, h = n;
+        int m;
+        while(l <= h){
+            m = l+(h-l)/2;
+            int p = guess(m);
+            if(p == 0)
+                return m;
+            if(p == -1)
+                h = m-1;
+            else
+                l = m+1;
+        }
+        
+        return -1;
+    }
+};
