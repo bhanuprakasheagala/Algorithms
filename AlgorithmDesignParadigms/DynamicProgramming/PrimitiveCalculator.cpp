37b9c33e62213ffb2f930c84ed7ddd050231e258
Bhanuprakash Eagala <31948251+bhanuprakasheagala@users.noreply.github.com>
2025-05-04 10:20:40 +0530

    Primitive Calculator problem using DP

--- AlgorithmDesignParadigms/DynamicProgramming/PrimitiveCalculator.cpp
+++ AlgorithmDesignParadigms/DynamicProgramming/PrimitiveCalculator.cpp
@@ -1,3 +1,37 @@
+/*
+Find the minimum number of operations needed to get a positive integer nfrom 1 by using only three operations: add 1, multiply by 2, and multiply by 3.
+Input: An integer n.
+Output: The minimum number of operations “+1”, “×2”, and “×3” needed to get n from 1.
+
+
+Input format. An integer n.
+Output format. In the first line, output the minimum number kof operations needed to get nfrom 1. In the second line,
+output a sequence of intermediate numbers. That is, the second line should contain positive integers a0,a1,...,ak such that
+a0 = 1, ak = n and for all 1≤i≤k,
+ai is equal to either ai−1 + 1, 2ai−1, or 3ai−1. If there are many such sequences, output any one of them.
+
+Constraints: 1≤n≤106
+
+Sample 1.
+Input:
+1
+Output:
+0
+1
+
+
+Sample 2.
+Input:
+96234
+Output:
+14
+1 3 9 10 11 22 66 198 594 1782 5346 16038 16039 32078 96234
+
+Another valid output in this case is “1 3 9 10 11 33 99 297 891 2673
+8019 16038 16039 48117 96234”
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <climits>
